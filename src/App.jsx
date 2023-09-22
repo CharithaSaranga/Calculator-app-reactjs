77f2812d87bb0637b7f07ff26003a1b0102aa20c
Charitha Saranga <108354430+CharithaSaranga@users.noreply.github.com>
2023-09-22 22:13:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import Calculator from "./calculator";
+
+function App() {
+  return <Calculator />;
+}
+
+export default App;
---
+++ src/calculator.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./styles.css";
+function Calculator() {
+  const [inputValue, setInputValue] = useState("");
+
+  function display(value) {
+    setInputValue(inputValue + value);
+  }
+
+  function calculate() {
+    let answers = eval(inputValue);
+    setInputValue(answers);
+  }
+
+  function clear() {
+    setInputValue("");
+  }
+  return (
+    <>
+      <form className="calculator" name="calc">
+        <h2>Calculator App</h2>
+
+        <input type="text" className="value input-field" value={inputValue} />
+        <span className="clear" onClick={() => clear()}>
+          C
+        </span>
+
+        <span onClick={() => display("/")}>/</span>
+        <span onClick={() => display("*")}>*</span>
+        <span onClick={() => calculate()} className="num equal">
+          =
+        </span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("-")}>-</span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("6")}>6</span>
+        <span onClick={() => display("+")} className="plus">
+          +
+        </span>
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("3")}>3</span>
+        <span onClick={() => display("0")}>0</span>
+        <span onClick={() => display("00")}>00</span>
+        <span onClick={() => display(".")}>.</span>
+      </form>
+    </>
+  );
+}
+
+export default Calculator;
